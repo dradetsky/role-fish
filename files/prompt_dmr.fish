f2ad878e440102a481983e4df9e528b0d0cd605f
Daniel Radetsky <dradetsky@gmail.com>
2022-08-01 17:07:04 -0700

    ttc

--- files/prompt_dmr.fish
+++ files/prompt_dmr.fish
@@ -1,8 +1,17 @@
 function prompt_dmr
     set -g last_status $status
 
-    set -l delim '$ '
-    fish_is_root_user; and set delim '# '
+    if not set -q fish_color_host_id
+        set -U fish_color_host_id (text_to_color $hostname)
+    end
+
+    set -l normal (set_color normal)
+    set -l delimcolor (set_color $fish_color_host_id)
+
+    set -l delim_inner '$ '
+    fish_is_root_user; and set delim_inner '# '
+
+    set -l delim "$delimcolor$delim_inner$normal"
 
     set -l prompt_from_right
     if test $skip_right_prompt -eq 1
--- files/text_to_color.fish
+++ files/text_to_color.fish
@@ -1,5 +1,17 @@
+function text_to_hash_1
+    set -l shas (echo $argv | cksum -a blake2b | string split -f4 ' ' | string sub -e 6)
+    echo $shas
+end
+
+alias text_to_hash='text_to_hash_1'
+
 function text_to_color
-    set -l shas (echo $argv | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 | string match -ra ..)
+    set -l hash (text_to_hash $argv)
+    hash_to_color $hash
+end
+
+function hash_to_color
+    set -l shas (string match -ra .. $argv)
     set -l col 0x$shas[1..3]
 
     # If the (simplified idea of) luminance is below 120 (out of 255), add some more.
@@ -13,9 +25,17 @@ function text_to_color
     echo -n -s $col
 end
 
-function text_to_color_demo
-    set -l normal (set_color normal)
-    set -l col (text_to_color (pwd -P))
-    set -l delim '$ '
-    echo -n -s (set_color $col) $delim $normal
+function text_to_hash_0
+    set -l shas (echo $argv | cksum | string split -f1 ' ' | math --base=hex | string sub -s 3 -e 8)
+    echo $shas
+end
+
+function text_to_color_2
+    set -l hash (text_to_hash_2 $argv)
+    hash_to_color $hash
+end
+
+function text_to_hash_2
+    set -l shas (echo $argv | cksum -a sha512 | string split -f4 ' ' | string sub -e 6)
+    echo $shas
 end
