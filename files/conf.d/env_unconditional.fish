2f46c296a69811e07dfc99d3cb130bf18766519f
Daniel Radetsky <dradetsky@gmail.com>
2022-08-22 13:48:44 -0700

    more

---
+++ files/conf.d/env_unconditional.fish
@@ -0,0 +1,3 @@
+set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
+
+set -x GOPATH ~/code/go
--- files/config.fish
+++ files/config.fish
@@ -24,4 +24,14 @@ if status is-interactive
     fish_add_path ~/bin
     fish_add_path ~/.emacs.d/bin
     fish_add_path ~/.npm/bin
+    fish_add_path ~/code/go/bin
+
+    alias emg='emacsclient -a /bin/false -c'
+    alias emc='emacsclient -a /bin/false --tty'
+
+    abbr -a gpnc git pull --no-commit
+    abbr -a gd git diff
+    abbr -a gcd1 git clone --depth 1
 end
+
+source (status dirname)/conf.d/env_unconditional.fish
